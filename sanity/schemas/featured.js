0c8c6ec2a5012044e61a274d5733cfd8afeddb96
Mar-Issah <masy370@gmail.com>
2022-11-09 20:26:34 -0400

    feat: create schema for featured

---
+++ sanity/schemas/featured.js
@@ -0,0 +1,26 @@
+export default {
+  name: 'featured',
+  title: 'Featured menu categorires',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Featured category name',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'short_description',
+      type: 'string',
+      title: 'Short description',
+      validation: (Rule) => Rule.max(200),
+    },
+    {
+      name: 'restaurants',
+      type: 'array', //array of res reference type of res.js
+      of: [{ type: 'reference', to: [{ type: 'restaurant' }] }],
+      title: 'Dishes',
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+};
