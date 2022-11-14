d753ef5f4128631cb519d373f8adb7cd347101fa
Mar-Issah <masy370@gmail.com>
2022-11-14 14:56:00 -0400

    feat: create restaurant screen

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -11,7 +11,7 @@ const Categories = () => {
       setCategories(data);
     });
   }, []);
-  console.log(categories);
+  // console.log(categories);
   return (
     // horizonatal scrollview
     //no arrow
