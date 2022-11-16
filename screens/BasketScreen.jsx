f62c29b41e466853beecc3ddc4f32f149780f811
Mar-Issah <masy370@gmail.com>
2022-11-16 19:47:47 -0400

    feat: build basket screen header

--- screens/BasketScreen.jsx
+++ screens/BasketScreen.jsx
@@ -1,16 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useMemo, useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { selectRestaurant, selectedBasketItems } from '../redux/selectors';
 import { useDispatch, useSelector } from 'react-redux';
+import colours from '../config/colours';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { useRoute, useNavigation } from '@react-navigation/native';
 
 const BasketScreen = () => {
   const [groupedItemBasket, setGroupedItemBasket] = useState([]);
   const restaurant = useSelector(selectRestaurant);
   const items = useSelector(selectedBasketItems);
 
+  const navigation = useNavigation();
   const dispatch = useDispatch();
 
-  useMemo(() => {
+  useEffect(() => {
     //memoised data,
     //return results after creating an object with the key as the id and group each item in an array based on the key
     const groupedItems = items.reduce((results, item) => {
@@ -18,15 +22,53 @@ const BasketScreen = () => {
       return results;
     }, {});
     setGroupedItemBasket(groupedItems);
-  }, [items]);
-  console.log(groupedItemBasket);
+  }, []);
+  // console.log(restaurant);
   return (
-    <View>
-      <Text>BasketScreen</Text>
-    </View>
+    <SafeAreaView style={styles.AndroidSafeArea}>
+      <View style={styles.headingContainer}>
+        <View>
+          <Text style={styles.heading}>Basket</Text>
+          <Text style={styles.normalText}>{restaurant.title}</Text>
+        </View>
+        <TouchableOpacity style={styles.closeContainer} onPress={() => navigation.goBack()}>
+          <Icon name='close' size={20} color={colours.location} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default BasketScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  AndroidSafeArea: {
+    backgroundColor: colours.offWhite,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    paddingHorizontal: 15,
+  },
+  headingContainer: {
+    backgroundColor: colours.white,
+    justifyContent: 'center',
+  },
+  closeContainer: {
+    position: 'absolute',
+    right: 10,
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: colours.primary,
+    textAlign: 'center',
+  },
+  normalText: {
+    color: colours.primary,
+    fontSize: 14,
+    marginVertical: 4,
+    textAlign: 'center',
+  },
+  boldText: {
+    fontWeight: 'bold',
+    color: colours.primary,
+  },
+});
